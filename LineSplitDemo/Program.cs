ce6fdedaf914cf318de94b3f6faa58d513ed1edd
lenidar <55443074+lenidar@users.noreply.github.com>
2023-04-19 16:51:33 +0800

    Add project files.

---
+++ LineSplitDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineSplitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string thing = "The quick brown fox jumps over the lazy dog       ";
+            string[] words = thing.Split(' ');
+
+            Console.WriteLine(words.Length);
+
+            foreach(string word in words) 
+            {
+                Console.Write("\"");
+                Console.Write(word);
+                Console.WriteLine("\"");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
